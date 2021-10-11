4058dd6bd97919e9c6454be0992f1154fdcd8b60
mariapaulaloaiza <58571961+mariapaulaloaiza@users.noreply.github.com>
2021-10-11 07:48:05 -0500

    Add files via upload

---
+++ segundaentrega/vfscliente.py
@@ -0,0 +1,87 @@
+import zmq #provee la comunicacion a traves de sockets (hulk)
+import sys
+import base64
+import os
+from shasum import *
+from shasumtexto import *
+from crearServidores import *
+import json
+archivo={}
+
+servidores=crearServidores()
+rangos=rangos(servidores)
+
+
+context = zmq.Context()#black box!!
+link=' '
+tamanoArchivo=0
+llave=''
+
+def recibir():
+    print("recibiendo...")
+    m=s.recv_multipart()
+    orden=[m[0].decode("utf-8"),m[1]]
+    image_64_decode = base64.decodebytes(orden[1])
+    image_result = open('C:\\Users\\Usuario\\Documents\\utp\\arquitectura\\segundaentrega\\'+orden[0], 'ab')#Cambiar con respecto al usuario,escritura y binario
+    image_result.write(image_64_decode)
+    size_file = os.path.getsize('C:\\Users\\Usuario\\Documents\\utp\\arquitectura\\segundaentrega\\'+orden[0])
+    mensaje='documento cargado'
+    print(size_file)
+
+def enviar(username,orden,nombreArchivo,direccion):
+    #Crea un socket y lo conecta a traves del protocolo tcp con el equipo local en el puerto 8001
+    s = context.socket(zmq.REQ)
+
+    f = open(direccion, 'rb')# R lee el archivo en modo binario B
+    while True:
+        archivoLeido = f.read(1024*1024)
+        if not archivoLeido:
+            image_64_encode = base64.encodebytes(b'0')
+            break
+        image_64_encode = base64.encodebytes(archivoLeido)
+        llaveDelArchivo=shasumtexto(image_64_encode)
+        numDelServidor=1
+        for r in rangos:
+            if r.member(int(llaveDelArchivo,16)):
+                print('llave Del Archivo: ',llaveDelArchivo,' al rango: ',r.toStr(),' servidor: ', numDelServidor)
+                break
+            numDelServidor+=1
+        archivo[llaveDelArchivo]=numDelServidor
+        s.connect('tcp://localhost:'+str(8000+numDelServidor))
+        #                [m[0].decode("utf-8"),m[1].decode("utf-8"),m[2].decode("utf-8"),m[3]]
+        s.send_multipart([orden.encode(),nombreArchivo.encode(),llaveDelArchivo.encode(),image_64_encode])
+        respuesta=s.recv_multipart()
+        print(respuesta)
+    f.close()
+
+
+username = sys.argv[1]
+orden = sys.argv[2]
+nombreArchivo = sys.argv[3]
+
+if orden=='upload':
+    direccion='C:\\Users\\Usuario\\Documents\\utp\\arquitectura\\segundaentrega\\'+nombreArchivo
+    enviar(username,orden,nombreArchivo,direccion)
+    with open(nombreArchivo+'.json','w') as file:
+        json.dump(archivo,file)
+    file.close()
+    print(archivo)
+else:
+    if orden=='download':
+        #lee el archivo... lo extrae y lo mete en el diccionario archivo
+        with open(nombreArchivo+'.json') as file:
+            archivo = json.load(file) #cargo el archivo json
+        file.close()
+        s = context.socket(zmq.REQ)
+        for key, value in archivo.items():
+            s.connect('tcp://localhost:'+str(8000+value))
+            s.send_multipart([orden.encode(),nombreArchivo.encode(),str(key).encode(),str(value).encode()])
+            recibir()
+    if orden=='list':
+        mensaje=' '
+        nombreServidorAConsultar=nombreArchivo
+        s = context.socket(zmq.REQ)
+        s.connect('tcp://localhost:'+str(8000+int(nombreServidorAConsultar)))
+        s.send_multipart([orden.encode(),nombreServidorAConsultar.encode(),mensaje.encode(),mensaje.encode()])
+        respuesta=s.recv_multipart()
+        print(respuesta[0].decode("utf-8"))
---
+++ segundaentrega/vfsservidor.py
@@ -0,0 +1,43 @@
+import zmq
+import os
+import base64
+context = zmq.Context()
+s= context.socket(zmq.REP)
+s.bind('tcp://*:8001') #protocol://*:puerto     * significa:
+
+def enviar(orden,nombreArchivo,key,value):
+    f = open('C:\\Users\\Usuario\\Documents\\utp\\arquitectura\\segundaentrega\\'+'servidor1'+'\\'+key, 'rb')# R lee el archivo en modo binario B
+    print('enviando...')
+    archivoLeido = f.read(1024*1024)
+    image_64_encode = base64.encodebytes(archivoLeido)
+    s.send_multipart([nombreArchivo.encode(),image_64_encode])
+    f.close()
+
+
+def recibir(orden):
+    print("recibiendo...")
+    image_64_decode = base64.decodebytes(orden[3])
+    image_result = open('C:\\Users\\Usuario\\Documents\\utp\\arquitectura\\segundaentrega\\servidor1\\'+orden[2], 'ab')#Cambiar con respecto al usuario,escritura y binario
+    image_result.write(image_64_decode)
+    size_file = os.path.getsize('C:\\Users\\Usuario\\Documents\\utp\\arquitectura\\segundaentrega\\servidor1\\'+orden[2])
+    mensaje='documento cargado'+ str(size_file)
+    print('cantidad cargada: ',size_file)
+    s.send_multipart([mensaje.encode()])
+
+while True:
+    print("calculando...")
+    m=s.recv_multipart()
+    #     [orden.encode(),nombreArchivo.encode(),llaveDelArchivo.encode(),image_64_encode]
+    orden=[m[0].decode("utf-8"),m[1].decode("utf-8"),m[2].decode("utf-8"),m[3]]
+    if orden[0]=='upload':
+        recibir(orden)
+    if orden[0]=='download' or orden[1]=='downloadlink':
+        #[orden.encode(),nombreArchivo.encode(),key.encode(),value.encode()]
+        enviar(orden[0],orden[1],orden[2],orden[3].decode("utf-8"))
+    if orden[0]=='sharelink':
+        mensaje='C:\\Users\\Usuario\\Documents\\utp\\arquitectura\\segundaentrega'+orden[1]+'.json'
+        s.send_multipart([mensaje.encode()])
+    if orden[0]=='list':
+        list = os.listdir("C:\\Users\\Usuario\\Documents\\utp\\arquitectura\\segundaentrega\\servidor1") # dir is your directory path
+        mensaje="archivos: "+ str(list)
+        s.send_multipart([mensaje.encode()])
